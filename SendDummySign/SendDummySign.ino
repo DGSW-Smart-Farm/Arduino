d5d1686f337c6bc2cc2e220993ceefedd323b672
ahjeon0407 <63032730+ahjeon0407@users.noreply.github.com>
2021-04-28 20:20:50 +0900

    [Update] 주석 작성
    주석을 추가로 작성하였다.

--- SendDummySign/SendDummySign.ino
+++ SendDummySign/SendDummySign.ino
@@ -17,13 +17,13 @@
 //  ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 // 
 
-unsigned int gndHumi_value = 500;
-unsigned int gndAcid_value = 70;
-unsigned int airHumi_value = 500;
-unsigned int airTemp_value = 500;
-unsigned int airCO2_value  = 1000;
-bool ledStat = true;
-bool ledTime = false;
+unsigned int gndHumi_value = 500; //토양 수분
+unsigned int gndAcid_value = 70; //토양 산성도
+unsigned int airHumi_value = 500; //공기중 습도
+unsigned int airTemp_value = 500; //공기중 온도
+unsigned int airCO2_value  = 1000; //이산화탄소
+bool ledStat = true; //led 상태
+bool ledTime = false; //led 작동시간
 
 
 void setup(){
@@ -37,7 +37,7 @@ void loop(){
     print_lines();
 }
 
-void changevalueRandom(){
+void changevalueRandom(){ 
     gndHumi_value += random(-10, 11);
     gndAcid_value += random(-10, 11);
     airHumi_value += random(-10, 11);
@@ -47,7 +47,7 @@ void changevalueRandom(){
     ledTime += random(-10, 11);
 }
 
-void print_lines(){
+void print_lines(){ //상태 출력
     Serial.println("==========================");
     print_line("gndHumi : ", gndHumi_value);
     print_line("gndAcid : ", gndAcid_value);
@@ -60,7 +60,7 @@ void print_lines(){
     delay(1000);
 }
 
-void print_line(String a, unsigned int b) {
+void print_line(String a, unsigned int b) { 
     Serial.print(a);
     Serial.println(b);
 }
