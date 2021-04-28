5e7a133ec695cfe81f71f33076f9c5b2b0ecf06f
KimJihun-1315 <65662529+KimJihun-1315@users.noreply.github.com>
2021-04-29 07:21:57 +0900

    주석보기 좋게 편집

--- SendDummySign/SendDummySign.ino
+++ SendDummySign/SendDummySign.ino
@@ -17,13 +17,13 @@
 //  ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 // 
 
-unsigned int gndHumi_value = 500; //토양 수분
-unsigned int gndAcid_value = 70; //토양 산성도
-unsigned int airHumi_value = 500; //공기중 습도
-unsigned int airTemp_value = 500; //공기중 온도
-unsigned int airCO2_value  = 1000; //이산화탄소
-bool ledStat = true; //led 상태
-bool ledTime = false; //led 작동시간
+unsigned int gndHumi_value = 500;   //토양 수분
+unsigned int gndAcid_value = 70;    //토양 산성도
+unsigned int airHumi_value = 500;   //공기중 습도
+unsigned int airTemp_value = 500;   //공기중 온도
+unsigned int airCO2_value  = 1000;  //이산화탄소
+bool ledStat = true;    //led 상태
+bool ledTime = false;   //led 작동시간
 
 
 void setup(){
@@ -47,7 +47,7 @@ void changevalueRandom(){
     ledTime += random(-10, 11);
 }
 
-void print_lines(){ //상태 출력
+void print_lines(){     //상태 출력
     Serial.println("==========================");
     print_line("gndHumi : ", gndHumi_value);
     print_line("gndAcid : ", gndAcid_value);
