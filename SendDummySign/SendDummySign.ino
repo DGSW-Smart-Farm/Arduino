044cababba62e6049e759334ded9240043b9453e
KimJihun-1315 <65662529+KimJihun-1315@users.noreply.github.com>
2021-04-28 19:53:41 +0900

    Dev_SendDummySign

---
+++ SendDummySign/SendDummySign.ino
@@ -0,0 +1,66 @@
+// 
+// 센서의 값은 소숫점 한자리 이하로 나타낸다.
+// 소숫점이하 자리의 값을 표현하는 방법은 10을 곱하여 나타낸다
+//      ex) 11.5`C => 115
+//      ex) 52.7 % => 527
+
+//  ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+//   Senser                 | Data Type      | Unit           | Etc                       
+//  ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+//   gnd_Humidity           | unsingned int  | percent        | 0~1000 (/10)
+//   gnd_Acidity            | unsingned int  | ph             | 10~140 (/10)
+//   air_Humidity           | unsingned int  | percent        | 0~1000 (/10)
+//   air_Temperature        | unsingned int  | degree celcius | 0~1000 (/10)
+//   air_carbonDioxide      | unsingned int  | ppm            | 0~2000 (none *10)
+//   led_Status             | bool           | NONE           | True=On False=Off
+//   led_OnTime             | bool           | NONE           | True=Start False=end
+//  ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+// 
+
+unsigned int gndHumi_value = 500;
+unsigned int gndAcid_value = 70;
+unsigned int airHumi_value = 500;
+unsigned int airTemp_value = 500;
+unsigned int airCO2_value  = 1000;
+bool ledStat = true;
+bool ledTime = false;
+
+
+void setup(){
+    Serial.begin(9600);
+    Serial.flush(); 
+    Serial.println("Start Serial Printing");
+}
+
+void loop(){
+    changevalueRandom();
+    print_lines();
+}
+
+void changevalueRandom(){
+    gndHumi_value += random(-10, 11);
+    gndAcid_value += random(-10, 11);
+    airHumi_value += random(-10, 11);
+    airTemp_value += random(-10, 11);
+    airCO2_value += random(-10, 11);
+    ledStat += random(-10, 11);
+    ledTime += random(-10, 11);
+}
+
+void print_lines(){
+    Serial.println("==========================");
+    print_line("gndHumi : ", gndHumi_value);
+    print_line("gndAcid : ", gndAcid_value);
+    print_line("airHumi : ", airHumi_value);
+    print_line("airTemp : ", airTemp_value);
+    print_line("air_CO2 : ", airCO2_value);
+    print_line("ledStat : ", ledStat);
+    print_line("ledTime : ", ledTime);
+    Serial.println("==========================\n");
+    delay(1000);
+}
+
+void print_line(String a, unsigned int b) {
+    Serial.print(a);
+    Serial.println(b);
+}
